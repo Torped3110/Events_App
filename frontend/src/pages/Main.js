f1df078abbb113dbd0f08974b06d05a5573031ff
Tarun Reddy <tarunnallavalli@gmail.com>
2023-07-26 22:09:19 +0530

    after protected routing

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,31 +1,34 @@
 import Navbar from "../components/Navbar";
 import {Button, Card} from '@mantine/core'
+import { useEffect } from "react";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 import { useHistory, useNavigate } from "react-router-dom";
 function Home(){   
     const d= useSelector(state=>state.page)
     const e = useSelector(state=>state.login)
     const navigate=useNavigate()
- 
-    if(!e)
-    {
+    useEffect( ()=>{
+
+        if(!e)
+        {
+           // not working
+           //console.log("if",e)
+            navigate('/')
+        }
+            
+    })
+        if(e){
         return(
             <div>
-                <h1>You are not logged in </h1>
-                <Button onClick={()=>{navigate('/')}}>Go to Login Page</Button>
-            </div>)
-    }
-    else{
-    return(
-        <div>
-        <header>
-        <Navbar/>
-        </header>
-        <div className="content" style={{zIndex:0,position:'relative'}}>
-        <Card className="Card" style={{zIndex:0}}>{d}</Card>
-        </div>
-        </div>
-    )}
+            <header>
+            <Navbar/>
+            </header>
+            <div className="content" style={{zIndex:0,position:'relative'}}>
+            <Card className="Card" style={{zIndex:0}}>{d}</Card>
+            </div>
+            </div>
+        )}
+    
 }
 
 export default Home;
